500baae9cd766a419cb3e8e9e829d1e8eb82bd1f
Daniel Naylor <93977310+drnaylor-oc@users.noreply.github.com>
2022-04-21 10:51:40 +0100

    CTCP-249: Add run_all_tests, make config entry a lazy val

--- app/uk/gov/hmrc/transitmovementsvalidator/config/AppConfig.scala
+++ app/uk/gov/hmrc/transitmovementsvalidator/config/AppConfig.scala
@@ -23,5 +23,5 @@ import play.api.Configuration
 @Singleton
 class AppConfig @Inject() (config: Configuration) {
 
-  val appName: String = config.get[String]("appName")
+  lazy val appName: String = config.get[String]("appName")
 }
