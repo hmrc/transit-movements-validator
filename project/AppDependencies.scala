34c1539c2a3212e2848cdfb4b44bbedf51546dc1
Derek Palmer <35488891+derekspalmer@users.noreply.github.com>
2022-07-20 11:14:58 +0100

    CTCP-656: following dev discussion remove play-json-schema-validator

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,9 +6,8 @@ object AppDependencies {
   val catsVersion = "2.7.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"  % "5.24.0",
-    "org.typelevel"     %% "cats-core"                  % catsVersion,
-    "com.eclipsesource" %% "play-json-schema-validator" % "0.9.5"
+    "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % "5.24.0",
+    "org.typelevel" %% "cats-core"                 % catsVersion
   )
 
   val test = Seq(
