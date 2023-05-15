9ac672fa5ed3b27876edb5133ce6f6b200c9c72f
olakunlekuye2 <olakunle.kuye2@digital.hmrc.gov.uk>
2023-05-15 12:11:29 +0100

    Updated message type checking

--- app/uk/gov/hmrc/transitmovementsvalidator/services/XmlValidationService.scala
+++ app/uk/gov/hmrc/transitmovementsvalidator/services/XmlValidationService.scala
@@ -122,7 +122,6 @@ class XmlValidationServiceImpl @Inject() (implicit ec: ExecutionContext) extends
       }
     }
 
-
   override def businessRuleValidation(messageType: String, source: Source[ByteString, _])(implicit
                                                                                           materializer: Materializer,
                                                                                           ec: ExecutionContext
@@ -289,6 +288,7 @@ class XmlValidationServiceImpl @Inject() (implicit ec: ExecutionContext) extends
       }
 
     }
+
 
   def buildParser(messageType: MessageType): SAXParserFactory = {
     val schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
