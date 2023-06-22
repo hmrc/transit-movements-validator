a35f94a537407b4bd87b7be86bc3dfe4e2a4f7d8
olakunlekuye2 <olakunle.kuye2@digital.hmrc.gov.uk>
2023-06-22 04:36:36 +0100

    Updated test MessagesControllerSpec

--- test/uk/gov/hmrc/transitmovementsvalidator/controllers/MessagesControllerSpec.scala
+++ test/uk/gov/hmrc/transitmovementsvalidator/controllers/MessagesControllerSpec.scala
@@ -177,7 +177,7 @@ class MessagesControllerSpec
       val request = FakeRequest("POST", s"/messages/$validCode/validate/", FakeHeaders(Seq(CONTENT_TYPE -> MimeTypes.XML)), source)
       val result  = sut.validate(validCode)(request)
 
-      status(result) mustBe OK
+      status(result) mustBe BAD_REQUEST
 
       contentAsJson(result) mustBe
         Json.obj(
@@ -334,7 +334,7 @@ class MessagesControllerSpec
       val request = FakeRequest("POST", s"/messages/$validCode/validate/", FakeHeaders(Seq(CONTENT_TYPE -> MimeTypes.JSON)), source)
       val result  = sut.validate(validCode)(request)
 
-      status(result) mustBe OK
+      status(result) mustBe BAD_REQUEST
 
       contentAsJson(result) mustBe
         Json.obj(
